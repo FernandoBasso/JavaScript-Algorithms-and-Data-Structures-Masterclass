1c5268ef3554cd677d11b687b63b774e1e4f7432
Fernando Basso <fernandobasso.br@gmail.com>
2021-01-30 12:28:04 -0300

    Add another countUniqVals solution
    I did this on my own too, and then took some tips from the video and
    improved the code a little bit.

--- 05-problem-solving-patterns/count-uniq-vals.mjs
+++ 05-problem-solving-patterns/count-uniq-vals.mjs
@@ -25,8 +25,6 @@ function countUniqVals_v1(nums) {
   return count;
 }
 
-export { countUniqVals_v1 as countUniqVals };
-
 //
 // === <1> ===
 // ===========
@@ -45,3 +43,63 @@ export { countUniqVals_v1 as countUniqVals };
 // loop in case we are sure to have at least two values in the array and
 // that comparison with undefined would not happen.
 //
+
+
+/**
+ * Count unique values in the input array of numbers.
+ *
+ * ASSUME: The input is sorted and does not contain empty slots.
+ *
+ * @param {Array<number>} nums
+ * @return {NaN|number}
+ */
+function countUniqVals_v2(nums) {
+  if (!Array.isArray(nums)) return NaN;
+  if (nums.length === 0) return 0;
+
+  let i = 0, j = 1;
+
+  while (j < nums.length) {
+    if (nums[i] !== nums[j]) { // <1>
+      ++i;
+      nums[i] = nums[j]; // <2>
+    }
+    j++;
+  }
+
+  // <3>
+  return i + 1;
+}
+
+//
+// This solution is based on the tips before Colt actually shows any
+// code. It involves changing the array and moving all the different
+// unique values one after the other to the beginning (left side) of the
+// array. When the loop stops, i is at the position of the largest value
+// found.
+//
+// === <1> ===
+// ===========
+// This solution doesn't attempt to compare with undefined because we
+// are looping and checking j, which already starts at 1. If the input
+// has  length 1, then j (which starts at 1) is not less that
+// nums.length and we don't compare numbers against undefined.
+//
+// === <2> ===
+// ===========
+// We do not need to swap the elements. In this case, we don't care
+// about the state of the array after the computation is performed. We
+// just want the number of unique values. It is enough to move [j] to
+// the place of updated [i].
+//
+// === <3> ===
+// ===========
+// Because arrays start at index 0, and i stops at the last element that
+// was different, we have to add 1 to it to return the correct result.
+//
+// Basically what we return is the length of the portion that we moved
+// to the beginning of the array.
+//
+
+export { countUniqVals_v2 as countUniqVals };
+
