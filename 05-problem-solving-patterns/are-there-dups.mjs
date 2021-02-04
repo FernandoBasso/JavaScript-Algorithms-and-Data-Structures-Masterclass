86ac3b55909551cb58263aefbfc33a973d320487
Fernando Basso <fernandobasso.br@gmail.com>
2021-02-04 08:09:35 -0300

    Add multiple pointers solution to areThereDups
    Include my solution plus a solution from the instructor of the course.
    Beware of some `Array.prototype.sort` stuff with V8. See comments in the
    source file.

--- 05-problem-solving-patterns/are-there-dups.mjs
+++ 05-problem-solving-patterns/are-there-dups.mjs
@@ -49,7 +49,76 @@ function areThereDups_v1(...args) {
   return false;
 }
 
+/**
+ * Checks if there are any duplicate values among the arguments.
+ *
+ * Solution using the “multiple pointers” pattern.
+ *
+ * @param {...(number|string|nul|undefined)}
+ * @return {boolean}
+ *
+ * <1> Since the input is not sorted, I do not know how to use two
+ * pointers and move them in such a way that less iterations are
+ * required.
+ *
+ */
+function areThereDups_v2(...args) {
+  if (args.length === 0) return false;
+
+  let l = 0, r = args.length - 1;
+
+  while (l < r) // <1>
+    if (args[l++] === args[r])
+      return true;
+
+  return false;
+}
+
+/**
+ * The solution from the instructor. He first sorts the array in order
+ * to make it possible to move both pointers. I did not sort the array
+ * because that is a somewhat expensive operation nonetheless.
+ *
+ * This solution doesn't work well with Node because of some
+ * Array.prototype.sort() change in V8:
+ *
+ * • https://github.com/nodejs/node/pull/22754
+ *
+ * Looks like if we change the `>` in the original sort function by the
+ * instructor:
+ *
+ *   (a, b) => a > b
+ *
+ * to `-` it works in Node too:
+ *
+ *   (a, b) => a - b
+ */
+function areThereDups_v3(...args) {
+  ////
+  // > doesn't work in V8 anymore for this sort thing.
+  //
+  // args.sort((a, b) => a > b);
+  //
+  args.sort((a, b) => a - b);
+
+  let start = 0;
+  let next = 1;
+
+  while(next < args.length) {
+    if (args[start] === args[next]) {
+        return true;
+    }
+
+    start++;
+    next++;
+  }
+
+  return false;
+}
+
 export {
   areThereDups_v1,
+  areThereDups_v2,
+  areThereDups_v3,
 };
 
