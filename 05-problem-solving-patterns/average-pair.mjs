43cc2dc2c045121baa20321f42382dfc862a7f84
Fernando Basso <fernandobasso.br@gmail.com>
2021-02-06 13:55:39 -0300

    Better format last commited files

--- 05-problem-solving-patterns/average-pair.mjs
+++ 05-problem-solving-patterns/average-pair.mjs
@@ -1,7 +1,9 @@
 //
 // https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/quiz/4410602
 //
-// Multiple Pointers - averagePairWrite a function called averagePair.
+// Multiple Pointers - avgPair
+//
+// Write a function called avgPair.
 // Given a sorted array of integers and a target average, determine if
 // there is a pair of values in the array where the average of the pair
 // equals the target average. There may be more than one pair that
@@ -14,10 +16,10 @@
 //
 // Sample Input:
 //
-// averagePair([1,2,3],2.5); // true
-// averagePair([1,3,3,5,6,7,10,12,19],8); // true
-// averagePair([-1,0,3,4,5,6], 4.1) // false
-// averagePair([],4); // false
+// avgPair([1,2,3],2.5); // true
+// avgPair([1,3,3,5,6,7,10,12,19],8); // true
+// avgPair([-1,0,3,4,5,6], 4.1) // false
+// avgPair([],4); // false
 //
 
 /**
