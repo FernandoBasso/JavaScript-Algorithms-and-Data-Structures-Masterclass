a610c91db6f1e55d2ea98994809198d693d3d53d
Fernando Basso <fernandobasso.br@gmail.com>
2021-02-08 06:43:40 -0300

    isSubSeq using multiple pointers approach

---
+++ 05-problem-solving-patterns/is-subseq.spec.mjs
@@ -0,0 +1,19 @@
+import {
+  isSubSeq_v1 as isSubSeq,
+} from './is-subseq';
+
+describe('isSubSeq()', () => {
+  it('should return true', () => {
+    expect(isSubSeq('dc', 'abcd')).toBe(false);
+    expect(isSubSeq('abc', 'acb')).toBe(false);
+    expect(isSubSeq('abcde', 'abcd')).toBe(false);
+    expect(isSubSeq('may the force', 'be with you')).toBe(false);
+  });
+
+  it('should return true', () => {
+    expect(isSubSeq('force', 'may the force be with you!')).toBe(true);
+    expect(isSubSeq('ana', 'banana')).toBe(true);
+    expect(isSubSeq('abc', 'abracadabra')).toBe(true);
+    expect(isSubSeq('sing', 'sting')).toBe(true);
+  });
+});
