f385b6e7a36ff6da1a4e85eb2bb2d9d20d15b3b8
Fernando Basso <fernandobasso.br@gmail.com>
2021-02-16 09:49:35 -0300

    arrayProduct solution

---
+++ 08-recursion-challenges/array-product.spec.mjs
@@ -0,0 +1,33 @@
+import {
+  arrayProduct,
+} from './array-product';
+
+describe('arrayProduct()', () => {
+  it('should return 0 when array is empty', () => {
+    expect(arrayProduct([])).toEqual(1);
+  });
+
+  it('should return n when array has a single element n', () => {
+    expect(arrayProduct([0])).toEqual(0);
+    expect(arrayProduct([1])).toEqual(1);
+    expect(arrayProduct([2e5])).toEqual(2e5);
+  });
+
+  it('should add all the elements of the array', () => {
+    expect(arrayProduct([-3, 3])).toEqual(-3 * 3); // -9
+    expect(arrayProduct([-3, 3, 1])).toEqual(-3 * 3 * 1); // -9
+    expect(arrayProduct([-3, 3, -1])).toEqual(-3 * 3 * -1); // 9
+
+    expect(
+      arrayProduct([1, 1, 1, 1, 1])
+    ).toEqual(1 * 1 * 1 * 1 * 1) // 1
+
+    expect(
+      arrayProduct([5, 1, 2])
+    ).toEqual(5 * 1 * 2) // 10
+
+    expect(
+      arrayProduct([5, 1, 0, 2])
+    ).toEqual(5 * 1 * 0 * 2) // 0
+  });
+});
