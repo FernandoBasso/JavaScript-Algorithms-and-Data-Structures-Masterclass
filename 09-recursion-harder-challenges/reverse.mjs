6b64d6b3fa5a6b2bbe9b263e56bfcadcdee301ba
Fernando Basso <fernando.basso@compasso.com.br>
2021-03-03 15:52:46 -0300

    reverse string recursive implementation

---
+++ 09-recursion-harder-challenges/reverse.mjs
@@ -0,0 +1,56 @@
+//
+// https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/quiz/425976
+//
+// Write a recursive function called reverse which accepts a string and
+// returns a new string in reverse.
+//
+
+
+/**
+ * Reverses a reversed version of `s`.
+ *
+ * ASSUME: input is a valid string.
+ *
+ * This solution involves appending the last element of the string to
+ * the accumulator array until there are not more chars in the input
+ * string, which keeps shrinking until we reach the base case.
+ *
+ * @param {string} s
+ * @return {string}
+ */
+function reverse_v1(s, acc = []) {
+  const len = s.length;
+  if (len === 0) return acc.join('');
+  return reverse_v1(s.slice(0, len - 1), [...acc, s[len - 1]]);
+}
+
+
+/**
+ * Reverses a reversed version of `s`.
+ *
+ * ASSUME: input is valid.
+ *
+ * This solution uses the internal accumulator trick. It keeps
+ * recursively calling itself until the input string `s` is empty, in
+ * which case we reach the base case.
+ *
+ * @param {string} s
+ * @return string
+ */
+function reverse_v2(s) {
+  let reversed = [];
+  let len = s.length;
+
+  if (len === 0) return [];
+
+  reversed.push(s[len - 1]);
+
+  reversed = reversed.concat(reverse_v2(s.slice(0, len - 1)))
+
+  return reversed.join('');
+}
+
+export {
+  reverse_v1,
+  reverse_v2,
+};
