fc014746a21f7bb68e53da71b5385d054e0bdac9
Fernando Basso <fernandobasso.br@gmail.com>
2021-03-14 09:33:29 -0300

    Uppercase first char of each string in list

---
+++ 09-recursion-harder-challenges/capitalize-first.spec.mjs
@@ -0,0 +1,26 @@
+import {
+  // capitalizeFirst_v1 as capitalizeFirst,
+  capitalizeFirst_v2 as capitalizeFirst,
+} from './capitalize-first';
+
+describe('capitalizeFirst()', () => {
+  describe('when input is an empty array', () => {
+    it('should return an empty array', () => {
+      expect(capitalizeFirst([])).toEqual([]);
+    });
+  });
+
+  describe('when input contains one or more strings', () => {
+    it('should capitalize the first char of each string', () => {
+      expect(capitalizeFirst(['car'])).toEqual(['Car']);
+
+      expect(
+        capitalizeFirst(['car', 'banana'])
+      ).toEqual(['Car', 'Banana']);
+
+      expect(
+        capitalizeFirst(['car', 'banana', 'x', 'linux'])
+      ).toEqual(['Car', 'Banana', 'X', 'Linux']);
+    });
+  });
+});
